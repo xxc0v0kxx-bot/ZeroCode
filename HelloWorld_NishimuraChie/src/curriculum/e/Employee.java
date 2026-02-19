9b08060f66c0cf607003a482fd24dd3c5e81327d
user <user@DESKTOP-J7QR09U>
2026-02-19 10:09:34 +0900

    ①基本概念　提出

---
+++ HelloWorld_NishimuraChie/src/curriculum/e/Employee.java
@@ -0,0 +1,10 @@
+package curriculum.e;
+
+public class Employee {
+
+	String name;
+
+	public void work(String name) {
+		System.out.println(name + "は働いています。");
+	}
+}
---
+++ HelloWorld_NishimuraChie/src/curriculum/e/Main.java
@@ -0,0 +1,12 @@
+package curriculum.e;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Employee employee = new Employee();
+		employee.work("山田");
+
+	}
+
+}
